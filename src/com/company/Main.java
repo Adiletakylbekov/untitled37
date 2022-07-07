6353a4ad2a6c60b64c61fa68b8d53cda49ecfe68
user <adilerakulbekov940@gmail.ru>
2022-07-07 15:58:47 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Ychbyrchtyk ychbyrchtyk = new Ychbyrchtyk();
+        ychbyrchtyk.a = (5);
+        ychbyrchtyk.b = (4);
+        ychbyrchtyk.c = (8);
+
+        ychbyrchtyk.area();
+
+        System.out.println(ychbyrchtyk.a);
+        System.out.println(ychbyrchtyk.b);
+        System.out.println(ychbyrchtyk.c);
+    }
+}
---
+++ src/com/company/Ychbyrchtyk.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Ychbyrchtyk {
+
+
+
+    int a;
+    int b;
+    int c;
+
+
+    public void  area() {
+        double P=(a+b+c)/2d;
+        System.out.println(Math.sqrt(P*(P-a)*(P-b)*(P-c)));
+
+
+
+    }
+
+}
